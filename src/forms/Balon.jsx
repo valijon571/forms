b296b0f639236ac2136b56b00e941cd1093d3abf
valijon571 <qarshiboyevv85@gmail.com>
2023-12-01 11:45:24 +0500

    update

--- src/forms/Balon.jsx
+++ src/forms/Balon.jsx
@@ -4,59 +4,102 @@ import { BalonStyle } from "./BalonStyle";
 const Balon = () => {
   return (
     <>
-      <div class="content">
-        <div class="form_target">
-          <div class="sc-jaXxmE_gwJYWD">
-            <div class="sc-cmaqmh_suTSs">
-              <div class="head">
-                <div class="sc-kAyceB_iZppJd">
-                  <b>Заполнение</b> персональных данных
+      <BalonStyle>
+        <div class="content">
+          <div class="form_target">
+            <div class="sc-jaXxmE_gwJYWD">
+              <div class="sc-cmaqmh_suTSs">
+                <div class="head">
+                  <div class="sc-kAyceB_iZppJd">
+                    <b>Заполнение</b> персональных данных
+                  </div>
                 </div>
-              </div>
-              <div class="f_body">
-                <div class="sub_title">Beneficiary data</div>
-                <form>
-                  <div class="row">
-                    <div class="doc_number">
-                      <label>Passport seria/nubmer</label>
-                      <div class="inputs">
-                        <div class="seria">
-                          <label class="sc-gsFSXq_dVuJAY" for="passportSeries">
+                <div class="f_body">
+                  <div class="sub_title">Beneficiary data</div>
+                  <form>
+                    <div class="row">
+                      <div class="doc_number">
+                        <label>Passport seria/nubmer</label>
+                        <div class="inputs">
+                          <div class="seria">
+                            <label
+                              class="sc-gsFSXq_dVuJAY"
+                              for="passportSeries"
+                            >
+                              <div class="i_target">
+                                <div class="input_body">
+                                  <input
+                                    placeholder="AB"
+                                    name="passportSeries"
+                                    value=""
+                                  />
+                                </div>
+                              </div>
+                            </label>
+                          </div>
+                          <div class="number">
+                            <label
+                              class="sc-gsFSXq_dVuJAY"
+                              for="passportNumber"
+                            >
+                              <div class="i_target">
+                                <div class="input_body">
+                                  <input
+                                    placeholder="1234567"
+                                    name="passportNumber"
+                                    value=""
+                                  />
+                                </div>
+                              </div>
+                            </label>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col_2">
+                        <div className="on">
+                          <label class="sc-gsFSXq_dVuJAY" for="birthDate">
+                            <div class="label">Birthdate</div>
                             <div class="i_target">
                               <div class="input_body">
                                 <input
-                                  placeholder="AB"
-                                  name="passportSeries"
+                                  type="date"
+                                  name="birthDate"
+                                  placeholder=""
                                   value=""
                                 />
                               </div>
                             </div>
                           </label>
                         </div>
-                        <div class="number">
-                          <label class="sc-gsFSXq_dVuJAY" for="passportNumber">
+                        <div className="to">
+                          <label class="sc-gsFSXq_dVuJAY" for="phone">
+                            <div class="label">Phone</div>
                             <div class="i_target">
                               <div class="input_body">
-                                <input
-                                  placeholder="1234567"
-                                  name="passportNumber"
-                                  value=""
-                                />
+                                <input placeholder="" name="phone" value="" />
+                              </div>
+                            </div>
+                          </label>
+                        </div>
+                        <div className="sri">
+                          <label class="sc-gsFSXq_dVuJAY" for="inn">
+                            <div class="label">Inn</div>
+                            <div class="i_target">
+                              <div class="input_body">
+                                <input placeholder="" name="inn" value="" />
                               </div>
                             </div>
                           </label>
                         </div>
                       </div>
-                    </div>
-                    <div class="col_2">
                       <div>
-                        <label class="sc-gsFSXq_dVuJAY" for="birthDate">
-                          <div class="label">Birthdate</div>
+                        <label class="sc-gsFSXq_dVuJAY" for="address">
+                          <div class="label">Address</div>
                           <div class="i_target">
                             <div class="input_body">
                               <input
-                                type="date"
-                                name="birthDate"
+                                type="text"
+                                name="address"
                                 placeholder=""
                                 value=""
                               />
@@ -64,55 +107,19 @@ const Balon = () => {
                           </div>
                         </label>
                       </div>
-                      <div>
-                        <label class="sc-gsFSXq_dVuJAY" for="phone">
-                          <div class="label">Phone</div>
-                          <div class="i_target">
-                            <div class="input_body">
-                              <input placeholder="" name="phone" value="" />
-                            </div>
-                          </div>
-                        </label>
-                      </div>
-                      <div>
-                        <label class="sc-gsFSXq_dVuJAY" for="inn">
-                          <div class="label">Inn</div>
-                          <div class="i_target">
-                            <div class="input_body">
-                              <input placeholder="" name="inn" value="" />
-                            </div>
-                          </div>
-                        </label>
-                      </div>
                     </div>
-                    <div>
-                      <label class="sc-gsFSXq_dVuJAY" for="address">
-                        <div class="label">Address</div>
-                        <div class="i_target">
-                          <div class="input_body">
-                            <input
-                              type="text"
-                              name="address"
-                              placeholder=""
-                              value=""
-                            />
-                          </div>
-                        </div>
-                      </label>
+                    <div class="btns">
+                      <button type="submit" class="sc-imWYAI_fJvLDQ">
+                        Найти
+                      </button>
                     </div>
-                  </div>
-                  <div class="btns">
-                    <button type="submit" class="sc-imWYAI_fJvLDQ">
-                      Найти
-                    </button>
-                  </div>
-                </form>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
         </div>
-        
-      </div>
+      </BalonStyle>
     </>
   );
 };
--- src/forms/BalonStyle.jsx
+++ src/forms/BalonStyle.jsx
@@ -1,4 +1,339 @@
 import styled from "styled-components";
 export const BalonStyle = styled.div`
-    
-`
+  & .form_target {
+    width: 100%;
+    display: flex;
+    margin-top: 50px;
+    justify-content: center;
+    & .sc-cmaqmh_suTSs {
+      border-radius: 20px;
+      background: rgb(255, 255, 255);
+      box-shadow: rgba(0, 34, 125, 0.1) 0px 0px 20px 0px;
+      border-width: 0px;
+      border-style: solid;
+      box-sizing: border-box;
+      overflow-wrap: break-word;
+      width: 80%;
+      & .head {
+        padding: 12px 30px;
+        border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+        & .sc-kAyceB_iZppJd {
+          width: 100%;
+          display: block;
+          color: rgb(24, 24, 24);
+          font-size: 30px;
+          font-style: normal;
+          font-weight: 400;
+          line-height: 30px;
+          letter-spacing: 1.5px;
+          padding: 12px 0px;
+        }
+      }
+      & .f_body {
+        padding: 0px 30px;
+        & .sub_title {
+          color: rgb(24, 24, 24);
+          font-size: 22px;
+          font-style: normal;
+          font-weight: 700;
+          line-height: 25px;
+          letter-spacing: 1.1px;
+          padding-top: 20px;
+        }
+        & .row {
+          border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+          & .doc_number {
+            max-width: 600px;
+            & label {
+              color: rgb(24, 24, 24);
+              font-size: 18px;
+              font-style: normal;
+              font-weight: 700;
+              line-height: 18.2px;
+              letter-spacing: 0.9px;
+              display: block;
+              padding-top: 14px;
+            }
+            & .inputs {
+              display: flex;
+              & .seria {
+                width: 30%;
+                padding-right: 20px;
+                max-width: 130px;
+                min-width: 100px;
+                & .sc-gsFSXq_dVuJAY {
+                  color: rgb(24, 24, 24);
+                  font-size: 18px;
+                  font-style: normal;
+                  font-weight: 700;
+                  line-height: 18.2px;
+                  letter-spacing: 0.9px;
+                  display: block;
+                  padding-top: 14px;
+                  & .i_target {
+                    display: flex;
+                    width: 100%;
+                    -webkit-box-align: center;
+                    align-items: center;
+                    & .input_body {
+                      border-radius: 10px;
+                      border: 1px solid rgb(207, 219, 224);
+                      padding: 16px 18px;
+                      width: 100%;
+                      display: flex;
+                      -webkit-box-align: center;
+                      align-items: center;
+                      & input {
+                        color: rgb(0, 0, 0);
+                        font-size: 20px;
+                        font-style: normal;
+                        font-weight: 400;
+                        line-height: 27px;
+                        letter-spacing: 1px;
+                        width: 100%;
+                        border: none;
+                      }
+                    }
+                  }
+                }
+              }
+              & .number {
+                width: 100%;
+                padding-right: 20px;
+
+                & .sc-gsFSXq_dVuJAY {
+                  color: rgb(24, 24, 24);
+                  font-size: 18px;
+                  font-style: normal;
+                  font-weight: 700;
+                  line-height: 18.2px;
+                  letter-spacing: 0.9px;
+                  display: block;
+                  padding-top: 14px;
+
+                  & .i_target {
+                    display: flex;
+                    width: 100%;
+                    -webkit-box-align: center;
+                    align-items: center;
+                    & .input_body {
+                      border-radius: 10px;
+                      border: 1px solid rgb(207, 219, 224);
+                      padding: 16px 18px;
+                      width: 100%;
+                      display: flex;
+                      -webkit-box-align: center;
+                      align-items: center;
+                      & input {
+                        color: rgb(0, 0, 0);
+                        font-size: 20px;
+                        font-style: normal;
+                        font-weight: 400;
+                        line-height: 27px;
+                        letter-spacing: 1px;
+                        width: 100%;
+                        border: none;
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+          & .col_2 {
+            display: flex;
+            flex-wrap: wrap;
+            & .on {
+              padding-right: 20px;
+              width: 50%;
+              & .sc-gsFSXq_dVuJAY {
+                display: block;
+                width: 100%;
+                padding: 14px 0px;
+                & .label {
+                  color: rgb(24, 24, 24);
+                  font-size: 18px;
+                  font-style: normal;
+                  font-weight: 700;
+                  line-height: 18.2px;
+                  letter-spacing: 0.9px;
+                  display: block;
+                  padding-bottom: 14px;
+                }
+                & .i_target {
+                  display: flex;
+                  width: 100%;
+                  -webkit-box-align: center;
+                  align-items: center;
+                  & .input_body {
+                    border-radius: 10px;
+                    border: 1px solid rgb(207, 219, 224);
+                    padding: 16px 18px;
+                    width: 100%;
+                    display: flex;
+                    -webkit-box-align: center;
+                    align-items: center;
+                    & input {
+                      color: rgb(0, 0, 0);
+                      font-size: 20px;
+                      font-style: normal;
+                      font-weight: 400;
+                      line-height: 27px;
+                      letter-spacing: 1px;
+                      width: 100%;
+                      border: none;
+                    }
+                  }
+                }
+              }
+            }
+            & .to {
+              /* padding-right: 20px; */
+              /* width: 50%; */
+              & .sc-gsFSXq_dVuJAY {
+                display: block;
+                width: 100%;
+                padding: 14px 0px;
+                & .label {
+                  color: rgb(24, 24, 24);
+                  font-size: 18px;
+                  font-style: normal;
+                  font-weight: 700;
+                  line-height: 18.2px;
+                  letter-spacing: 0.9px;
+                  display: block;
+                  padding-bottom: 14px;
+                }
+                & .i_target {
+                  display: flex;
+                  width: 100%;
+                  -webkit-box-align: center;
+                  align-items: center;
+                  & .input_body {
+                    border-radius: 10px;
+                    border: 1px solid rgb(207, 219, 224);
+                    padding: 16px 18px;
+                    width: 100%;
+                    display: flex;
+                    -webkit-box-align: center;
+                    align-items: center;
+                    & input {
+                      color: rgb(0, 0, 0);
+                      font-size: 20px;
+                      font-style: normal;
+                      font-weight: 400;
+                      line-height: 27px;
+                      letter-spacing: 1px;
+                      width: 100%;
+                      border: none;
+                    }
+                  }
+                }
+              }
+            }
+            & .sri {
+              padding-right: 20px;
+              width: 50%;
+              & .sc-gsFSXq_dVuJAY {
+                display: block;
+                width: 100%;
+                padding: 14px 0px;
+                & .label {
+                  color: rgb(24, 24, 24);
+                  font-size: 18px;
+                  font-style: normal;
+                  font-weight: 700;
+                  line-height: 18.2px;
+                  letter-spacing: 0.9px;
+                  display: block;
+                  padding-bottom: 14px;
+                }
+                & .i_target {
+                  display: flex;
+                  width: 100%;
+                  -webkit-box-align: center;
+                  align-items: center;
+                  & .input_body {
+                    border-radius: 10px;
+                    border: 1px solid rgb(207, 219, 224);
+                    padding: 16px 18px;
+                    width: 100%;
+                    display: flex;
+                    -webkit-box-align: center;
+                    align-items: center;
+                    & input {
+                      color: rgb(0, 0, 0);
+                      font-size: 20px;
+                      font-style: normal;
+                      font-weight: 400;
+                      line-height: 27px;
+                      letter-spacing: 1px;
+                      width: 100%;
+                      border: none;
+                    }
+                  }
+                }
+              }
+            }
+          }
+          & .sc-gsFSXq_dVuJAY {
+            display: block;
+            width: 100%;
+            padding: 14px 0px;
+            & .label {
+              color: rgb(24, 24, 24);
+              font-size: 18px;
+              font-style: normal;
+              font-weight: 700;
+              line-height: 18.2px;
+              letter-spacing: 0.9px;
+              display: block;
+              padding-bottom: 14px;
+            }
+            & .i_target {
+              display: flex;
+              width: 100%;
+              -webkit-box-align: center;
+              align-items: center;
+              & .input_body {
+                border-radius: 10px;
+                border: 1px solid rgb(207, 219, 224);
+                padding: 16px 18px;
+                width: 100%;
+                display: flex;
+                -webkit-box-align: center;
+                align-items: center;
+                & input {
+                  color: rgb(0, 0, 0);
+                  font-size: 20px;
+                  font-style: normal;
+                  font-weight: 400;
+                  line-height: 27px;
+                  letter-spacing: 1px;
+                  width: 100%;
+                  border: none;
+                }
+              }
+            }
+          }
+        }
+        & .btns {
+          padding: 15px 0px 30px;
+          & button {
+            background: rgb(0, 170, 88);
+            color: rgb(255, 255, 255);
+            font-size: 20px;
+            font-style: normal;
+            font-weight: 700;
+            letter-spacing: 1px;
+            text-align: center;
+            padding: 16px 50px;
+            border-radius: 10px;
+            border: 1px solid rgb(0, 170, 88);
+            cursor: pointer;
+          }
+        }
+      }
+    }
+  }
+`;
