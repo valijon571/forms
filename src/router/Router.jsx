be6197216147f7abccfa5fc56e020bda8211d252
valijon571 <qarshiboyevv85@gmail.com>
2023-12-07 11:53:04 +0500

    update

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,8 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Step0 from "../forms/Step0";
 import Step2 from "../forms/Step2";
-import Step3 from "../forms/Step3";
-import Step4 from "../forms/Step4";
 
 
 const Router = () => {
@@ -11,8 +9,6 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Step0 />} />
         <Route path="/Step2" element={<Step2 />} />
-        <Route path="/Step3" element={<Step3 />} />
-        <Route path="/Step4" element={<Step4 />} />
 
 
       </Routes>
